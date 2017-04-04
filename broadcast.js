9d50af31edb4fb02fc04b4fc9287716ca9a90ca4
mikeshinoda <akin.park@gmail.com>
2017-04-04 10:16:09 +0530

    You Beauty!!!
    You Beauty!!!

--- broadcast.js
+++ broadcast.js
@@ -19,99 +19,97 @@ commander
 
 if(commander.list) {
   let questions = [
-	{
-		type: "input",
-		name: "sender.email",
-		message: "Sender's Email Address - "
-	},
-	{
-		type: "input",
-		name: "sender.name",
-		message: "Sender's Name - "
-	},
-	{
-		type: "input",
-		name: "subject",
-		message: "Subject "
-	}
-];
-let contactList = [];
-let parse = csv.parse;
-let stream = fs.createReadStream(commander.list)
-	.pipe(
-		parse(
-			{
-				delimiter: ','
-			}
-		)
-	);
+	  {
+		  type: "input",
+		  name: "sender.email",
+		  message: "Sender's Email Address - "
+	  },
+	  {
+	  	type: "input",
+		  name: "sender.name",
+		  message: "Sender's Name - "
+	  },
+	  {
+		  type: "input",
+		  name: "subject",
+		  message: "Subject "
+	  }
+  ];
+  let contactList = [];
+  let parse = csv.parse;
+  let stream = fs.createReadStream(commander.list)
+	  .pipe(
+		  parse(
+			  {
+				  delimiter: ','
+			  }
+		  )
+	  );
 
-let __sendEmail = function (to, from, subject, callback) {
-  let template = "Wishing you a Merry Christmas and a " +
-    "prosperous year ahead. P.S. Toby, I hate you.";
-  let helper = require('sendgrid').mail;
-  let fromEmail = new helper.Email(from.email, from.name);
-  let toEmail = new helper.Email(to.email, to.name);
-  let body = new helper.Content("text/plain", template);
-  let mail = new helper.Mail(fromEmail, subject, toEmail, body);
+  let __sendEmail = function (to, from, subject, callback) {
+    let template = "Glory Glory Man United..... And the reds go marching on and on and on!!!!";
+    let helper = require('sendgrid').mail;
+    let fromEmail = new helper.Email(from.email, from.name);
+    let toEmail = new helper.Email(to.email, to.name);
+    let body = new helper.Content("text/plain", template);
+    let mail = new helper.Mail(fromEmail, subject, toEmail, body);
 
-  let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-  let request = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: mail.toJSON(),
-  });
+    let sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    let request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
+    });
 
-  sg.API(request, function(error, response) {
-    if (error) { return callback(error); }
-    callback();
-  });
-};
+    sg.API(request, function(error, response) {
+      if (error) { return callback(error); }
+      callback();
+    });
+  };
 
-stream
-  .on("error", function (err) {
-    return console.error(err.response);
-  })
-  .on("data", function (data) {
-    let name = data[0] + " " + data[1];
-    let email = data[2];
-    contactList.push({ name : name, email : email });
-  })
-  .on("end", function () {
-    inquirer.prompt(questions).then(function (ans) {
-      async.each(contactList, function (recipient, fn) {
-        __sendEmail(recipient, ans.sender, ans.subject, fn);
-      }, function (err) {
-        if (err) {
-          return console.error(chalk.red(err.message));
-        }
-        console.log(chalk.green('Success'));
+  stream
+    .on("error", function (err) {
+      return console.error(err.response);
+    })
+    .on("data", function (data) {
+      let name = data[0] + " " + data[1];
+      let email = data[2];
+      contactList.push({ name : name, email : email });
+    })
+    .on("end", function () {
+      inquirer.prompt(questions).then(function (ans) {
+        async.each(contactList, function (recipient, fn) {
+          __sendEmail(recipient, ans.sender, ans.subject, fn);
+        }, function (err) {
+          if (err) {
+            return console.error(chalk.red(err.message));
+          }
+          console.log(chalk.green('Success'));
+        });
       });
     });
-  });
-
 } else if(commander.sms) {
   let twilQuestions = [
-  {
-    type: "input",
-    name: "twil.number",
-    message: "Recipient's Phone Number: "
-  },
-  {
-    type: "input",
-    name: "twil.body",
-    message: "SMS Body: "
-  }
-];
-var client = new twilio.RestClient(accountSid, authToken);
-inquirer.prompt(twilQuestions).then(function (ans) {
+    {
+      type: "input",
+      name: "twil.number",
+      message: "Recipient's Phone Number: "
+    },
+    {
+      type: "input",
+      name: "twil.body",
+      message: "SMS Body: "
+    }
+  ];
+  var client = new twilio.RestClient(accountSid, authToken);
+  inquirer.prompt(twilQuestions).then(function (ans) {
     client.messages.create({
-    body: ans.twil.body,
-    to: ans.twil.number,  // Text this number
-    from: '+17739806935' // From a valid Twilio number
-}, function(err, message) {
-  console.log(message);
-    console.log(message.sid);
-});
+      body: ans.twil.body,
+      to: ans.twil.number,  // Text this number
+      from: '+17739806935' // From a valid Twilio number
+    }, function(err, message) {
+      console.log(message);
+      console.log(message.sid);
     });
+  });
 }
